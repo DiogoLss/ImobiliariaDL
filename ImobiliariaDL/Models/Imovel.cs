761c4fb8dd2cd13e74498fe7e78eaa8d1f012f74
Diogo Lessa <diogodslessa@gmail.com>
2022-07-12 20:50:03 -0300

    Imovel Model

---
+++ ImobiliariaDL/Models/Imovel.cs
@@ -0,0 +1,15 @@
+﻿namespace ImobiliariaDL.Models
+{
+    public class Imovel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public string Imagens { get; set; }
+        public decimal Preco { get; set; }
+        public int Quartos { get; set; }
+        public int Banheiros { get; set; }
+        public int Salas { get; set; }
+        public int Garagens { get; set; }
+    }
+}

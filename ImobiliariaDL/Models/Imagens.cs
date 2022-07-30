e545c5084b83326017f213815b56b325cd244c8f
Diogo Lessa <diogodslessa@gmail.com>
2022-07-30 19:40:59 -0300

    data annotation

--- ImobiliariaDL/Models/Imagens.cs
+++ ImobiliariaDL/Models/Imagens.cs
@@ -1,8 +1,11 @@
-﻿namespace ImobiliariaDL.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ImobiliariaDL.Models
 {
     public class Imagem
     {
         public int Id { get; set; }
+        [Required]
         public string ImagemString { get; set; }
         public int ImovelId { get; set; }
         public Imovel Imovel { get; set; }

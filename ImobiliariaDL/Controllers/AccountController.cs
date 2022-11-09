aa28fbd714485a6ba66f61ffaf6f0806b75b5e55
Diogo Lessa <diogodslessa@gmail.com>
2022-11-08 21:16:04 -0300

    LoginController, Index e Post

---
+++ ImobiliariaDL/Controllers/AccountController.cs
@@ -0,0 +1,41 @@
+﻿using ImobiliariaDL.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImobiliariaDL.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        public AccountController(UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginVM login)
+        {
+            if(!ModelState.IsValid) return View(login);
+
+            var user = await _userManager.FindByNameAsync(login.UserName);
+
+            if(user != null)
+            {
+                var result = await _signInManager.PasswordSignInAsync(user, login.Password, false, false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Admin", "Index");
+                }
+            }
+            ModelState.AddModelError("", "Falha ao realizar login");
+            return View(login);
+        }
+    }
+}
---
+++ ImobiliariaDL/ViewModels/LoginVM.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ImobiliariaDL.ViewModels
+{
+    public class LoginVM
+    {
+        [Required(ErrorMessage = "Insira seu login ADM")]
+        [Display(Name = "Usuário")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Insiria sua senha ADM")]
+        [Display(Name = "Senha")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
